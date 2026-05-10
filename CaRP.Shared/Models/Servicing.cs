1fb54be5c454c428e58995bfd2840b3182949548
Szymon <szyfzjablonski@gmail.com>
2026-05-10 18:29:16 +0200

    entitiesy zapomniane

---
+++ CaRP.Shared/Models/Servicing.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CaRP.Shared.Models;
+
+public partial class Servicing
+{
+    public int Id { get; set; }
+
+    public string ServiceNumber { get; set; } = null!;
+
+    public int VehicleId { get; set; }
+
+    public string ClerkUsername { get; set; } = null!;
+
+    public string IssueDescription { get; set; } = null!;
+
+    public DateOnly ServiceDate { get; set; }
+
+    public string MechanicName { get; set; } = null!;
+
+    public decimal Cost { get; set; }
+
+    public virtual Vehicle Vehicle { get; set; } = null!;
+}
